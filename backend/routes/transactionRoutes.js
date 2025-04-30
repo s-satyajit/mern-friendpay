253614efbc77bd61afd6def6e0ae776b9def6273
Satyajit Samal <samalsatyajit668@gmail.com>
2025-04-30 10:20:55 +0530

    add transaction route and the index server

---
+++ backend/routes/transactionRoutes.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import Transaction from '../models/transaction';
+import Friend from '../models/friend';
+
+const router = express.Router();
+
+router.post('/', async(req, res) => {
+    try {
+
+    } catch (error) {
+
+    }
+})
+
+router.get('/', async (req, res) => {
+    try {
+
+    } catch (error) {
+        
+    }
+})
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import cors from 'cors';
+import transactionRouter from './routes/transactionRoutes.js';
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-
+app.use('/transactions', transactionRouter);
 
 const port = process.env.PORT || 3000;
 
