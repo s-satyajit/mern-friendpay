c6d1993c90aff8f9eeef7a0bd53733fc231bbd6f
Satyajit Samal <samalsatyajit668@gmail.com>
2025-04-30 19:16:16 +0530

    wip - add the transaction route for get and post taking the req.body from the frontend and updating db

--- backend/models/transaction.js
+++ backend/models/transaction.js
@@ -1,16 +1,19 @@
 import mongoose from "mongoose";
 
-const transactionSchema = new mongoose.Schema({
-  date: { type: Date, required: true },
-  friend: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Friend",
-    required: true,
+const transactionSchema = new mongoose.Schema(
+  {
+    date: { type: Date, required: true },
+    friend: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Friend",
+      required: true,
+    },
+    amount: { type: Number, required: true },
+    direction: { type: String, enum: ["paid", "received"], required: true },
+    dueDate: { type: Date, required: true },
   },
-  amount: {type: Number, required: true},
-  direction: {type: String, enum: ["paid", "received"], required: true},
-  date: {type: date, required: true},
-});
+  { timestamps: true }
+);
 
 const Transaction = mongoose.model("Transaction", transactionSchema);
 
